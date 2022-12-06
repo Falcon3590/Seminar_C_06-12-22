d4f4b181e45cf74543ba945c3f94f803b1cea993
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-06 22:44:28 +0300

    1

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿n = int(input("Введите число:"))
+temp = n
+rev = 0
+while(n > 0):
+    dig = n % 10
+    rev = rev * 10 + dig
+    n = n // 10
+if(temp == rev):
+    print("Это палиндром!")
+else:
+    print("Это не палиндром!")
