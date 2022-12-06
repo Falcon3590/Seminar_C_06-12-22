930173ffe669cc447aabfabfd6e0e4382ccb1d57
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-06 22:30:54 +0300

    Lesson.1

--- Lesson/Program.cs
+++ Lesson/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray (int[] arr, int len)
+
+{
+    for (int i=0; i < len; i++)
+     
+    {
+        arr[i] = new Random().Next(1, 11);
+    }
+}
+
+
+int N = Convert.ToInt32(Console.ReadLine());
+//1)
+int [] array = new int[N];
+
+//2)
+FillArray(array, N);
+
+//Name, return value, parameters
+
+//3)
+int sum = 0;
+float result = 0;
+int cuntOfEvens = 0;
+for (int i=0; i < N; i++)
+{
+    //1. Обратиться к четному элементу
+   if (array[i] % 2 == 0)
+    {
+        //2. прибавить его к сумме
+        sum += array [i];
+        //3.вести подчет четных чисел
+        cuntOfEvens++;
+
+    } 
+}
+
+result = (float)sum / cuntOfEvens;
+Console.WriteLine(result);
