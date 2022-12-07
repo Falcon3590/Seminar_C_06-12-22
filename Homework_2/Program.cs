0ad7bb9bcf72873bd8704f08bbdffced2dd0afea
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-08 00:32:10 +0300

    Homework_2

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,11 +1,13 @@
-﻿//Использую для вычесления формулу: Расстояние =  Корень из (x2-x1)^{2} + (y2-y1)^{2} + (z2-z1)^{2}
+﻿// Задача 2 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Использую для вычесления формулу: Расстояние =  Корень из (x2-x1)^{2} + (y2-y1)^{2} + (z2-z1)^{2}
 
 class PointsDis // метод или функция не может существовать без класса, поэтому создаю класс и даю ему название близкое к теме.
 {
      
-    static void distance(float x1, float y1, // использую модификатор static; тип возвращаемых данных "ничего"; название метода назначил сам "дистанция" 
-                     float z1, float x2,     // ввожу не целочисленные переменные 
-                     float y2, float z2)
+    static void distance(Double x1, Double y1, // использую модификатор static; тип возвращаемых данных "ничего"; название метода назначил сам "дистанция" 
+                     Double z1, Double x2,     // ввожу не целочисленные переменные 
+                     Double y2, Double z2)
     {
         double temp = Math.Pow((Math.Pow(x2 - x1, 2) +  //использую подсмотренную функцию Math.Pow которая извлекает квадратный корень.
                          Math.Pow(y2 - y1, 2) +
@@ -18,19 +20,40 @@ class PointsDis // метод или функция не может сущест
 // Код с указанием координат
 public static void Main()
 {
-    float x1 = 2;
-    float y1 = -5;
-    float z1 = 7;
-    float x2 = 3;
-    float y2 = 4;
-    float z2 = 5;
+    Console.WriteLine("Введите координаты х1=");
+    int coor_x1 = Int32.Parse(Console.ReadLine());
+    Double x1 = Convert.ToDouble(coor_x1);
      
-    // function call
-    // for distance
-    distance(x1, y1, z1,
+    Console.WriteLine("Введите координаты у1=");
+    int coor_y1= Int32.Parse(Console.ReadLine());
+    Double y1 = Convert.ToDouble(coor_y1);
+
+    Console.WriteLine("Введите координаты z1=");
+    int coor_z1 = Int32.Parse(Console.ReadLine());
+    Double z1 = Convert.ToDouble(coor_z1);
+
+    Console.WriteLine("Введите координаты х2=");
+    int coor_x2 = Int32.Parse(Console.ReadLine());
+    Double x2 = Convert.ToDouble(coor_x2);
+ 
+    Console.WriteLine("Введите координаты у2=");
+    int coor_y2 = Int32.Parse(Console.ReadLine());
+    Double y2 = Convert.ToDouble(coor_y2);
+    
+    Console.WriteLine("Введите координаты z2=");
+    int coor_z2= Int32.Parse(Console.ReadLine());
+    Double z2 = Convert.ToDouble(coor_z2);
+    
+    // Double x1 = 2; // можно хардкодом объявить переменную
+    // Double y1 = -5;
+    // Double z1 = 7;
+    // Double x2 = 3;
+    // Double y2 = 4;
+    // Double z2 = 5;
+     
+    
+    distance(x1, y1, z1,  // передаем полученное значение переменных в метод 
              x2, y2, z2);
 }
 }
  
-// This code is contributed
-// by chandan_jnu.
--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    Console.WriteLine("Введите координаты х1=");
+    int coor_x1 = Int32.Parse(Console.ReadLine());
+    Double x1 = Convert.ToDouble(coor_x1);
+     
+    Console.WriteLine("Введите координаты у1=");
+    int coor_y1= Int32.Parse(Console.ReadLine());
+    Double y1 = Convert.ToDouble(coor_y1);
+
+    Console.WriteLine("Введите координаты z1=");
+    int coor_z1 = Int32.Parse(Console.ReadLine());
+    Double z1 = Convert.ToDouble(coor_z1);
+
+    Console.WriteLine("Введите координаты х2=");
+    int coor_x2 = Int32.Parse(Console.ReadLine());
+    Double x2 = Convert.ToDouble(coor_x2);
+ 
+    Console.WriteLine("Введите координаты у2=");
+    int coor_y2 = Int32.Parse(Console.ReadLine());
+    Double y2 = Convert.ToDouble(coor_y2);
+    
+    Console.WriteLine("Введите координаты z2=");
+    int coor_z2= Int32.Parse(Console.ReadLine());
+    Double z2 = Convert.ToDouble(coor_z2);
