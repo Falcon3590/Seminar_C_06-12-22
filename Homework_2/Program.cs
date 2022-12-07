edf381105cdc9075eed7088a0c8c0d46484aeb13
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-08 00:43:44 +0300

    Homework_2

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -13,37 +13,31 @@ class PointsDis // метод или функция не может сущест
                          Math.Pow(y2 - y1, 2) +
                          Math.Pow(z2 - z1, 2) *
                                    1.0), 0.5);
-        Console.WriteLine("Расстояние между точками: " + temp);
+        Console.WriteLine("Расстояние между точками 1 и 2 = " + temp); // вывод результата на консоль
         return;
     }
  
 // Код с указанием координат
 public static void Main()
 {
-    Console.WriteLine("Введите координаты х1=");
-    int coor_x1 = Int32.Parse(Console.ReadLine());
-    Double x1 = Convert.ToDouble(coor_x1);
-     
+    Console.WriteLine("Введите координаты х1=");   // сразу решил принять значение переменных в Double (с плавающей запятой)
+    Double x1 = Double.Parse(Console.ReadLine());
+         
     Console.WriteLine("Введите координаты у1=");
-    int coor_y1= Int32.Parse(Console.ReadLine());
-    Double y1 = Convert.ToDouble(coor_y1);
-
+    Double y1= Double.Parse(Console.ReadLine());
+    
     Console.WriteLine("Введите координаты z1=");
-    int coor_z1 = Int32.Parse(Console.ReadLine());
-    Double z1 = Convert.ToDouble(coor_z1);
-
+    Double z1 = Double.Parse(Console.ReadLine());
+    
     Console.WriteLine("Введите координаты х2=");
-    int coor_x2 = Int32.Parse(Console.ReadLine());
-    Double x2 = Convert.ToDouble(coor_x2);
- 
+    Double x2 = Double.Parse(Console.ReadLine());
+     
     Console.WriteLine("Введите координаты у2=");
-    int coor_y2 = Int32.Parse(Console.ReadLine());
-    Double y2 = Convert.ToDouble(coor_y2);
-    
+    Double y2 = Double.Parse(Console.ReadLine());
+        
     Console.WriteLine("Введите координаты z2=");
-    int coor_z2= Int32.Parse(Console.ReadLine());
-    Double z2 = Convert.ToDouble(coor_z2);
-    
+    Double z2= Double.Parse(Console.ReadLine());
+        
     // Double x1 = 2; // можно хардкодом объявить переменную
     // Double y1 = -5;
     // Double z1 = 7;
@@ -57,3 +51,4 @@ public static void Main()
 }
 }
  
+// Как результат программа работает и чтото считает, предположительно значение между точками в условных единицах
